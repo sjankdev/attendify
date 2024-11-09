8aba182a9c07ff07779168f5b79bbd91cf92f671
sjankdev <stefanjanko.dev@gmail.com>
2024-11-09 23:11:47 +0100

    Initial commit

---
+++ attendify/src/test/java/com/app/attendify/AttendifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.attendify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
