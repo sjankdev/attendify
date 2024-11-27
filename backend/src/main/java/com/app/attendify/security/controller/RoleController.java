ac958f9880af1f8606d4c1b0d175dc153f931677
sjankdev <stefanjanko.dev@gmail.com>
2024-11-27 20:06:14 +0100

    Hosted on render

--- backend/src/main/java/com/app/attendify/security/controller/RoleController.java
+++ backend/src/main/java/com/app/attendify/security/controller/RoleController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -13,11 +15,22 @@ import java.util.List;
 @RestController
 public class RoleController {
 
-    @Autowired
-    private RoleService roleService;
+    private static final Logger logger = LoggerFactory.getLogger(RoleController.class);
+
+    private final RoleService roleService;
+
+    // Constructor Injection
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
 
     @GetMapping("/roles")
     public List<Role> getRoles() {
-        return roleService.getAllRoles();
+        List<Role> roles = roleService.getAllRoles();
+
+        // Log the roles fetched
+        logger.info("Fetched roles: {}", roles);
+
+        return roles;
     }
 }
