1a52b2cf77cb4f8bd8e64601c084a9628a69cba2
sjankdev <stefanjanko.dev@gmail.com>
2025-01-01 17:36:53 +0100

    Added a feature that allows event organizers to send bulk invitations to multiple emails instead of sending them one by one.

--- backend/src/main/java/com/app/attendify/company/dto/InvitationRequestDto.java
+++ backend/src/main/java/com/app/attendify/company/dto/InvitationRequestDto.java
@@ -3,31 +3,22 @@ package com.app.attendify.company.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.List;
+
 public class InvitationRequestDto {
 
-    @NotNull(message = "Email is required.")
-    @Email(message = "Please enter a valid email address.")
-    private String email;
+    @NotNull(message = "Emails are required.")
+    private List<EmailDepartment> emails;
 
     @NotNull(message = "Company ID is required.")
     private Integer companyId;
 
-    private Integer departmentId;
-
-    public Integer getDepartmentId() {
-        return departmentId;
+    public List<EmailDepartment> getEmails() {
+        return emails;
     }
 
-    public void setDepartmentId(Integer departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setEmails(List<EmailDepartment> emails) {
+        this.emails = emails;
     }
 
     public Integer getCompanyId() {
@@ -36,5 +27,29 @@ public class InvitationRequestDto {
 
     public void setCompanyId(Integer companyId) {
         this.companyId = companyId;
+    }
+
+    public static class EmailDepartment {
+        @Email(message = "Please enter a valid email address.")
+        @NotNull(message = "Email is required.")
+        private String email;
+
+        private Integer departmentId;
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Integer getDepartmentId() {
+            return departmentId;
+        }
+
+        public void setDepartmentId(Integer departmentId) {
+            this.departmentId = departmentId;
+        }
     }
 }
