2b97f4a8f0afad6d9ebc0120566f970b710f21c1
sjankdev <stefanjanko.dev@gmail.com>
2025-01-05 16:55:36 +0100

    Updated layout to use single background and changed colors on event organizer board

--- frontend/src/shared/components/Layout.tsx
+++ frontend/src/shared/components/Layout.tsx
@@ -12,9 +12,10 @@ import {
 
 interface LayoutProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const SidebarLayout: React.FC<LayoutProps> = ({ children }) => {
+const SidebarLayout: React.FC<LayoutProps> = ({ children, className }) => {
   const navigate = useNavigate();
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
@@ -23,9 +24,9 @@ const SidebarLayout: React.FC<LayoutProps> = ({ children }) => {
   };
 
   return (
-    <div className="min-h-screen bg-[#0d1b2a] flex flex-col lg:flex-row">
+    <div className="min-h-screen bg-[#471C7E] flex flex-col lg:flex-row">
       <div
-        className={`lg:w-64 w-full bg-[#1b263b] p-6 flex flex-col justify-between ${
+        className={`lg:w-64 w-full bg-[#471C7E] p-6 flex flex-col justify-between ${
           isSidebarOpen ? "block" : "hidden lg:block"
         }`}
       >
@@ -33,10 +34,10 @@ const SidebarLayout: React.FC<LayoutProps> = ({ children }) => {
           <h2 className="text-[#e0e1dd] text-2xl font-bold mb-10">Attendify</h2>
           <div
             onClick={() => navigate("/event-organizer")}
-            className="flex items-center space-x-4 p-4 rounded-xl bg-[#1b263b] shadow-md hover:shadow-lg hover:bg-[#0d1b2a] transition duration-300 cursor-pointer"
+            className="flex items-center space-x-4 p-4 rounded-xl bg-[#57239F] shadow-md hover:shadow-lg hover:bg-[#0d1b2a] transition duration-300 cursor-pointer"
           >
             <FaHome className="text-2xl text-teal-400" />
-            <span className="text-teal-400 text-lg font-medium">
+            <span style={{ color: "#fffcf2" }} className="text-lg font-medium">
               Event Organizer
             </span>
           </div>
@@ -44,50 +45,65 @@ const SidebarLayout: React.FC<LayoutProps> = ({ children }) => {
           <div className="space-y-6">
             <div
               onClick={() => navigate("/event-organizer/create-event")}
-              className="flex items-center space-x-4 p-4 rounded-xl bg-[#1b263b] shadow-md hover:shadow-lg hover:bg-[#0d1b2a] transition duration-300 cursor-pointer"
+              className="flex items-center space-x-4 p-4 rounded-xl bg-[#57239F] shadow-md hover:shadow-lg hover:bg-[#0d1b2a] transition duration-300 cursor-pointer"
             >
               <FaPlusCircle className="text-2xl text-teal-400" />
-              <span className="text-teal-400 text-lg font-medium">
+              <span
+                style={{ color: "#fffcf2" }}
+                className="text-lg font-medium"
+              >
                 Create Event
               </span>
             </div>
 
             <div
               onClick={() => navigate("/event-organizer/invitations")}
-              className="flex items-center space-x-4 p-4 rounded-xl bg-[#1b263b] shadow-md hover:shadow-lg hover:bg-[#0d1b2a] transition duration-300 cursor-pointer"
+              className="flex items-center space-x-4 p-4 rounded-xl bg-[#57239F] shadow-md hover:shadow-lg hover:bg-[#0d1b2a] transition duration-300 cursor-pointer"
             >
               <FaClipboardList className="text-2xl text-teal-400" />
-              <span className="text-teal-400 text-lg font-medium">
+              <span
+                style={{ color: "#fffcf2" }}
+                className="text-lg font-medium"
+              >
                 Manage Invitations
               </span>
             </div>
 
             <div
               onClick={() => navigate("/event-organizer/events")}
-              className="flex items-center space-x-4 p-4 rounded-xl bg-[#1b263b] shadow-md hover:shadow-lg hover:bg-[#0d1b2a] transition duration-300 cursor-pointer"
+              className="flex items-center space-x-4 p-4 rounded-xl bg-[#57239F] shadow-md hover:shadow-lg hover:bg-[#0d1b2a] transition duration-300 cursor-pointer"
             >
               <FaCalendarAlt className="text-2xl text-teal-400" />
-              <span className="text-teal-400 text-lg font-medium">
+              <span
+                style={{ color: "#fffcf2" }}
+                className="text-lg font-medium"
+              >
                 See Events
               </span>
             </div>
 
             <div
               onClick={() => navigate("/event-organizer/company-participants")}
-              className="flex items-center space-x-4 p-4 rounded-xl bg-[#1b263b] shadow-md hover:shadow-lg hover:bg-[#0d1b2a] transition duration-300 cursor-pointer"
+              className="flex items-center space-x-4 p-4 rounded-xl bg-[#57239F] shadow-md hover:shadow-lg hover:bg-[#0d1b2a] transition duration-300 cursor-pointer"
             >
               <FaUsers className="text-2xl text-teal-400" />
-              <span className="text-teal-400 text-lg font-medium">
+              <span
+                style={{ color: "#fffcf2" }}
+                className="text-lg font-medium"
+              >
                 Company Participants
               </span>
             </div>
 
             <div
               onClick={() => navigate("/event-organizer/company-departments")}
-              className="flex items-center space-x-4 p-4 rounded-xl bg-[#1b263b] shadow-md hover:shadow-lg hover:bg-[#0d1b2a] transition duration-300 cursor-pointer"
+              className="flex items-center space-x-4 p-4 rounded-xl bg-[#57239F] shadow-md hover:shadow-lg hover:bg-[#0d1b2a] transition duration-300 cursor-pointer"
             >
               <FaBuilding className="text-2xl text-teal-400" />
-              <span className="text-teal-400 text-lg font-medium">
+              <span
+                style={{ color: "#fffcf2" }}
+                className="text-lg font-medium"
+              >
                 Company Departments
               </span>
             </div>
@@ -109,7 +125,7 @@ const SidebarLayout: React.FC<LayoutProps> = ({ children }) => {
         />
       </div>
 
-      <div className="flex-1 p-6">{children}</div>
+      <div className={`flex-1 p-6 ${className}`}>{children}</div>
     </div>
   );
 };
