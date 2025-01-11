bf62aee478aa6c7b2006ebd30e9ec505cb78840f
sjankdev <stefanjanko.dev@gmail.com>
2025-01-11 20:29:50 +0100

    URLs for dev

--- frontend/src/security/components/Login.tsx
+++ frontend/src/security/components/Login.tsx
@@ -91,7 +91,7 @@ const Login: React.FC = () => {
 
     try {
       const response = await axios.post<LoginResponse>(
-        "https://attendify-backend-el2r.onrender.com/api/auth/login",
+        "http://localhost:8080/api/auth/login",
         { email, password }
       );
       localStorage.setItem("token", response.data.token);
@@ -124,7 +124,7 @@ const Login: React.FC = () => {
 
     try {
       const response = await axios.post(
-        "https://attendify-backend-el2r.onrender.com/api/auth/register-organizer",
+        "http://localhost:8080/api/auth/register-organizer",
         formData
       );
       setSuccess(
