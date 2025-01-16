a72391bb379324881ac4aa62311d186c40fe67f5
sjankdev <stefanjanko.dev@gmail.com>
2025-01-16 20:54:16 +0100

    Visually improved organizer dashboard

--- frontend/src/eventOrganizer/components/EventOrganizerPage.tsx
+++ frontend/src/eventOrganizer/components/EventOrganizerPage.tsx
@@ -56,17 +56,7 @@ const EventOrganizerPage: React.FC = () => {
         <h1 className="text-4xl font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600 mb-6">
           Event Organizer Dashboard
         </h1>
-
-        <div className="bg-gradient-to-r from-green-600 via-teal-600 to-blue-700 p-6 rounded-2xl shadow-lg">
-          <h2 className="text-2xl font-semibold text-white mb-4">
-            Expected Participants This Week
-          </h2>
-          <p className="text-3xl font-bold text-white">
-            {uniqueParticipantsCount} Participants
-          </p>
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
           <div className="bg-gradient-to-r from-indigo-600 via-blue-600 to-purple-700 p-6 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-2xl flex flex-col justify-between">
             <h2 className="text-2xl font-semibold mb-4 text-white">
               Upcoming Events This Week
@@ -92,7 +82,6 @@ const EventOrganizerPage: React.FC = () => {
                 </li>
               ))}
             </ul>
-
             {upcomingThisWeek.length > 5 && !showAllEvents && (
               <button
                 onClick={() => setShowAllEvents(true)}
@@ -109,6 +98,14 @@ const EventOrganizerPage: React.FC = () => {
                 See Less
               </button>
             )}
+          </div>
+          <div className="bg-gradient-to-r from-pink-600 via-red-600 to-orange-600 p-6 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-2xl flex flex-col justify-between">
+            <h2 className="text-2xl font-semibold text-white mb-4">
+              Expected Participants This Week
+            </h2>
+            <p className="text-3xl font-bold text-white">
+              {uniqueParticipantsCount} Participants
+            </p>
           </div>
         </div>
       </div>
