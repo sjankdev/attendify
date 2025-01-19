04e20d491b88f711a64d3ceb05b9b02394bc18be
sjankdev <stefanjanko.dev@gmail.com>
2025-01-19 21:21:35 +0100

    Added new section to display participants

--- frontend/src/eventOrganizer/components/EventDetailsPage.tsx
+++ frontend/src/eventOrganizer/components/EventDetailsPage.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { fetchEventDetailsWithParticipants } from "../services/eventOrganizerService";
+import {
+  fetchEventDetailsWithParticipants,
+  reviewJoinRequest,
+} from "../services/eventOrganizerService";
 import Layout from "../../shared/components/EventOrganizerLayout";
 import { Event } from "../../types/eventTypes";
 
@@ -9,7 +12,9 @@ const EventDetailsPage: React.FC = () => {
   const [eventDetails, setEventDetails] = useState<Event | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-
+  const [showAccepted, setShowAccepted] = useState<{
+    [eventId: number]: boolean;
+  }>({});
   useEffect(() => {
     const loadEventDetails = async () => {
       try {
@@ -47,6 +52,45 @@ const EventDetailsPage: React.FC = () => {
     );
   }
 
+  const toggleAcceptedParticipants = (eventId: number) => {
+    setShowAccepted((prev) => ({
+      ...prev,
+      [eventId]: !prev[eventId],
+    }));
+  };
+
+  const handleReviewJoinRequest = async (
+    eventId: number,
+    participantId: number,
+    status: "ACCEPTED" | "REJECTED"
+  ) => {
+    setLoading(true);
+    const success = await reviewJoinRequest(eventId, participantId, status);
+    setLoading(false);
+
+    if (success) {
+      setEventDetails((prevEventDetails) => {
+        if (!prevEventDetails) {
+          return prevEventDetails;
+        }
+
+        const updatedEvent = {
+          ...prevEventDetails,
+          participants: prevEventDetails.participants?.map((participant) => {
+            if (participant.participantId === participantId) {
+              return { ...participant, status };
+            }
+            return participant;
+          }),
+        };
+
+        return updatedEvent;
+      });
+    } else {
+      setError("Failed to update join request.");
+    }
+  };
+
   return (
     <Layout>
       <div className="max-w-3xl mx-auto p-6 bg-[#313030] rounded-lg shadow-lg">
@@ -82,35 +126,108 @@ const EventDetailsPage: React.FC = () => {
                   }`}
             </span>
           </div>
-          <div className="flex flex-col space-y-2">
-            <h3 className="text-xl font-semibold text-white">Participants</h3>
-            {Array.isArray(eventDetails.participants) &&
-            eventDetails.participants.length === 0 ? (
-              <p className="text-gray-500">No participants yet</p>
-            ) : (
-              <ul className="space-y-2">
-                {Array.isArray(eventDetails.participants) &&
-                  eventDetails.participants.map((participant, index) => (
-                    <li key={index} className="text-gray-300">
-                      <div className="flex items-center">
-                        <div className="mr-4">
-                          <strong className="text-sm text-gray-500">
-                            Name:
-                          </strong>{" "}
-                          {participant.participantName}
-                        </div>
-                        <div>
-                          <strong className="text-sm text-gray-500">
-                            Department:
-                          </strong>{" "}
-                          {participant.departmentName || "No Department"}
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-              </ul>
-            )}
-          </div>
+          {eventDetails.participants?.length > 0 && (
+            <div className="mt-6 border-t border-gray-600 pt-4">
+              <h4 className="text-lg font-semibold text-white">
+                Participants:
+              </h4>
+
+              <div className="mt-4">
+                <h5 className="text-white">
+                  Joined participants:{" "}
+                  {
+                    eventDetails.participants.filter(
+                      (p) => p.status === "ACCEPTED"
+                    ).length
+                  }
+                </h5>
+                <button
+                  className="mt-2 text-teal-600 hover:text-teal-500 focus:outline-none focus:ring-2 focus:ring-teal-500"
+                  onClick={() => toggleAcceptedParticipants(eventDetails.id)}
+                >
+                  {showAccepted[eventDetails.id] ? "Hide" : "View"} accepted
+                  participants
+                </button>
+              </div>
+
+              {showAccepted[eventDetails.id] &&
+                eventDetails.participants.filter((p) => p.status === "ACCEPTED")
+                  .length > 0 && (
+                  <ul className="space-y-4 mt-4">
+                    {eventDetails.participants
+                      .filter((p) => p.status === "ACCEPTED")
+                      .map((participant) => (
+                        <li
+                          key={participant.participantId}
+                          className="flex justify-between items-center bg-[#252525] p-3 rounded-lg shadow-md hover:shadow-lg transition-shadow"
+                        >
+                          <div>
+                            <p className="text-white">
+                              {participant.participantName}
+                            </p>
+                          </div>
+                        </li>
+                      ))}
+                  </ul>
+                )}
+
+              {eventDetails.participants.filter((p) => p.status === "PENDING")
+                .length > 0 && (
+                <div className="mt-6">
+                  <h5 className="text-white">Pending participants</h5>
+                  <ul className="space-y-4 mt-4">
+                    {eventDetails.participants
+                      .filter((p) => p.status === "PENDING")
+                      .map((participant) => (
+                        <li
+                          key={participant.participantId}
+                          className="flex justify-between items-center bg-[#252525] p-3 rounded-lg shadow-md hover:shadow-lg transition-shadow"
+                        >
+                          <div>
+                            <p className="text-white">
+                              {participant.participantName}
+                            </p>
+                          </div>
+                          <div>
+                            <span className="px-3 py-1 rounded-full text-sm font-medium bg-yellow-500 text-black">
+                              {participant.status}
+                            </span>
+                          </div>
+                          <div className="space-x-2">
+                            <button
+                              disabled={loading}
+                              onClick={() =>
+                                handleReviewJoinRequest(
+                                  eventDetails.id,
+                                  participant.participantId,
+                                  "ACCEPTED"
+                                )
+                              }
+                              className="bg-teal-600 text-white py-1 px-3 rounded-md hover:bg-teal-500 focus:outline-none focus:ring-2 focus:ring-teal-500"
+                            >
+                              Approve
+                            </button>
+                            <button
+                              disabled={loading}
+                              onClick={() =>
+                                handleReviewJoinRequest(
+                                  eventDetails.id,
+                                  participant.participantId,
+                                  "REJECTED"
+                                )
+                              }
+                              className="bg-red-600 text-white py-1 px-3 rounded-md hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-500"
+                            >
+                              Reject
+                            </button>
+                          </div>
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </Layout>
