b71178ea7a37a53ad6ba78ddf4235e65569085ed
sjankdev <stefanjanko.dev@gmail.com>
2025-01-23 19:17:48 +0100

    New colors of cards homepage

--- frontend/src/eventOrganizer/components/EventOrganizerPage.tsx
+++ frontend/src/eventOrganizer/components/EventOrganizerPage.tsx
@@ -81,7 +81,6 @@ const EventOrganizerPage: React.FC = () => {
       }}
     >
       <div className="flex flex-col p-8 space-y-12">
-        {/* Headline Section with two rows and custom font */}
         <div className="text-center space-y-4 mb-12">
           <h1
             className="text-4xl sm:text-5xl font-semibold leading-tight text-white"
@@ -95,7 +94,7 @@ const EventOrganizerPage: React.FC = () => {
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
-          <div className="bg-[#424CE0] p-6 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-2xl flex flex-col">
+          <div className="bg-[#11011E] p-6 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-2xl flex flex-col">
             <h2 className="text-2xl font-semibold text-white mb-4">
               Upcoming Events This Week
             </h2>
@@ -137,7 +136,7 @@ const EventOrganizerPage: React.FC = () => {
             )}
           </div>
 
-          <div className="bg-[#424CE0] p-6 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-2xl flex flex-col">
+          <div className="bg-[#11011E] p-6 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-2xl flex flex-col">
             <h2 className="text-2xl font-semibold text-white mb-4">
               Events in the Past Month
             </h2>
@@ -171,7 +170,7 @@ const EventOrganizerPage: React.FC = () => {
             )}
           </div>
 
-          <div className="bg-[#424CE0] p-6 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-2xl flex flex-col">
+          <div className="bg-[#11011E] p-6 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 hover:shadow-2xl flex flex-col">
             <h2 className="text-2xl font-semibold text-white mb-4">
               Expected Participants This Week
             </h2>
