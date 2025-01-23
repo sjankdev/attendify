9edb8358a4df0f029ecdee19347975009311b0c0
sjankdev <stefanjanko.dev@gmail.com>
2025-01-23 21:33:41 +0100

    Minor updates on color

--- frontend/src/eventOrganizer/components/EventDetailsPage.tsx
+++ frontend/src/eventOrganizer/components/EventDetailsPage.tsx
@@ -396,7 +396,7 @@ const EventDetailsPage: React.FC = () => {
                 className={`mt-4 px-6 py-2 rounded-lg shadow-md transition ease-in-out duration-200 ${
                   expandedFeedbacks[eventDetails.id]
                     ? "bg-indigo-500 text-white"
-                    : "bg-gray-700 text-gray-300"
+                    : "bg-[#6167E0] text-gray-300"
                 }`}
               >
                 {expandedFeedbacks[eventDetails.id]
